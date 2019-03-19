96748ac561e6375055552398daca52c6729f7e8e
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2019-03-19 16:37:17 -0300

    Update aula2_ex1.py

--- aula2_ex1.py
+++ aula2_ex1.py
@@ -3,16 +3,16 @@ altura = 12.0
 delimitador = "."
 
 print(largura/2)
-type(largura/2)
+print(type(largura/2))
 
 print(largura/2.0)
-type(largura/2.0)
+print(type(largura/2.0))
 
 print(altura/3)
-type(altura/3)
+print(type(altura/3))
 
 print(1 + 2 * 5)
-type(1 + 2 * 5)
+print(type(1 + 2 * 5))
 
 print(delimitador*5)
-type(delimitador*5)
+print(type(delimitador*5))

14fd4f6676efb9a97b61696c48b08a69624a4345
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-12 16:22:19 -0300

    Update aula1_ex3.py

--- aula1_ex3.py
+++ aula1_ex3.py
@@ -1,6 +1,12 @@
 import math
 
-# Para encontrar os zeros da função, utilizaremos a fórmula de Bháskara. Para a função dada, a = 3, b = -4, c = -10.
+# Declarando os coeficientes da equação
+a = 3
+b = -4
+c = -10
 
-print ((4 + math.sqrt(4*4 - 4*3*(-10)))/2*3)
-print ((4 - math.sqrt(4*4 - 4*3*(-10)))/2*3)
+# Encontrando as raízes através da fórmula de Bhaskara
+x1 = (-b + math.sqrt(b**2 -4*a*c))/2*a
+x2 = (-b - math.sqrt(b**2 -4*a*c))/2*a
+
+print(x1, x2, sep = ";")

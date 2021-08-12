2475b0a0cb8fd620e51a234893444f695fee7b98
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-12 16:13:59 -0300

    Update aula1_ex2.py

--- aula1_ex2.py
+++ aula1_ex2.py
@@ -1,3 +1,11 @@
-# O som demora 3 segundos a mais que a luz para chegar onde eu estou à velocidade v = 340 m/s. Portanto, a distância d em metros será d = v*ts - v*tl, onde ts é o tempo que o som demora para chegar em mim depois da luz e tl o tempo que a luz demora para chegar em mim. tl pode ser descrito como tl = v*ts/c
+# O tempo que a luz demora para chegar em mim é tl = d/c
+# O tempo que o som demora para chegar em mim é ts = d/v
+# O tempo em segundos entre as duas chegadas é ts-tl = 3
+# Manipulando as equações anteriores, obtemos a distância em metros: d = 3vc/(c-v)
 
-print (340*3 - 340*((340*3)/300000000))
+v=343
+c=3*10**8
+d = 3*v*c/(c-v)
+
+
+print (d)

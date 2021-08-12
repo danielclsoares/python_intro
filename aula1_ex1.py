8b3e67d7c00893f83489881846c58c07a41ec2e6
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-12 15:44:51 -0300

    Update aula1_ex1.py

--- aula1_ex1.py
+++ aula1_ex1.py
@@ -1,3 +1,5 @@
-# Se eu ando 10 km, eu ando (10/1,61) milhas. Portanto, o tempo médio por milha é [43,5/(10/1,61)] min/milha
-
-print (43.5/(10/1.61))
+# Se eu ando 10 km, eu ando (10/1,61) milhas. O tempo médio em horas é (43,5/60)h
+h=43.5/60
+m=10/1.61
+print (h/m)
+print (m/h)

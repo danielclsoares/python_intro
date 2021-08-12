b7831ec918fb5359c06d5570b47fc1496ea02f78
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-12 16:54:54 -0300

    Update aula1_ex6.py

--- aula1_ex6.py
+++ aula1_ex6.py
@@ -1,6 +1,17 @@
 import math
 
-# Um corpo em queda livre vai atingir o chão com velocidade v = sqrt(2*g*h), onde g é a aceleração da gravidade e h a altura na qual ele se encontra na situação inicial. O tempo no qual ele atinge o solo é t = v/g
+# Para saber a velocidade final de um corpo em queda livre,
+# Usamos a fórmula de Torricelli, com velocidade inicial 0m/s
 
-print (math.sqrt(2*9.8*3))
-print (math.sqrt(2*9.8*3)/9.8)
+# Declarando as variáveis:
+g = 9.8
+h = 3
+#onde g é a aceleração da gravidade e h a altura na qual ele se encontra na situação inicial.
+
+v = (math.sqrt(2*g*h))
+print(v)
+
+# O tempo decorrido, sai da equação horária da velocidade:
+
+t = v/g
+print(t)

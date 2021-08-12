f36cfdc7cf234f95077413679a6e5339cc2c9b44
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-12 15:45:20 -0300

    Update aula1_ex1.py

--- aula1_ex1.py
+++ aula1_ex1.py
@@ -1,4 +1,4 @@
-# Se eu ando 10 km, eu ando (10/1,61) milhas. O tempo médio em horas é (43,5/60)h
+# Se eu ando 10 km, eu ando (10/1,61) milhas. O tempo médio em horas é (43,5/60)
 h=43.5/60
 m=10/1.61
 print (h/m)

e4a07877db03007211f35988cda3ac06e2994125
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-08-25 16:06:22 -0300

    Update aula2_ex3.py

--- aula2_ex3.py
+++ aula2_ex3.py
@@ -1,7 +1,8 @@
 livro = 24.95 # Preço do livro
-desconto = (2*livro)/5 # Preço do livro com desconto
+desconto = livro - 0.4*livro # Preço do livro com desconto
+
 custo_de_envio = 0.75 
 n_livros = 60 #Número de livros pedidos
-custo_total = n_livros*(desconto + custo_de_envio)+ (3-.75)
+custo_total = n_livros*(desconto + custo_de_envio)+ (3- .75)
 
 print(custo_total)

c92a8747d90a89ede9330296072074fb646b668d
danielclsoares <48491946+danielclsoares@users.noreply.github.com>
2021-09-01 12:45:50 -0300

    Create aula5_ex6.py

---
+++ aula5/aula5_ex6.py
@@ -0,0 +1,29 @@
+import turtle
+
+jn = turtle.Screen()
+
+cor_fundo = str(input("digite a cor do fundo:"))
+jn.bgcolor(cor_fundo)
+
+joana = turtle.Turtle()
+
+joana.up()
+joana. goto(-100,0)
+joana.down()
+joana.color("blue")
+joana.pensize(3)
+joana.shape("turtle")
+joana.speed(3)
+
+for j in [0, 1, 2, 3]:
+
+  for i in [0, 1, 2, 3, 4]:
+  
+    joana.forward( 50 )
+    joana.right( 180 - 36 )
+  
+  joana.up()
+  joana.forward( 70 )
+  joana.down()
+  
+jn.mainloop()
